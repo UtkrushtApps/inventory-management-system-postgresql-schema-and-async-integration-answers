df10308766004eb1d7f8cdb8135b14606cd24e57
Naman Muley <naman.g.muley@gmail.com>
2025-08-02 18:12:08 +0530

    Add app/models.py

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Text, Numeric, TIMESTAMP, func
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Product(Base):
+    __tablename__ = 'products'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False, unique=True, index=True)
+    description = Column(Text, nullable=True)
+    quantity = Column(Integer, nullable=False, default=0)
+    price = Column(Numeric(10,2), nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=func.now())
+    updated_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=func.now())
